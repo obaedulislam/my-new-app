4cecac0faa429ce2db8045057baf59698dcf3f8b
obayedul islam <obaed.cse@gmail.com>
2023-01-04 19:00:27 +0600

    Header section Done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+import { SessionProvider } from "next-auth/react";
+import Header from "../Components/Header/Header";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <div>
+      <SessionProvider session={session}>
+        <Header></Header>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </div>
+  );
 }
